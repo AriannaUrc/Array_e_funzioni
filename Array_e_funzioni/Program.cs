a0e9bffee1c4166afd71e6a14ae0f48ac926b628
URCIUOLI.22381 <URCIUOLI.22381@LAB2-13PC20>
2023-01-13 12:20:46 +0100

    Aggiungere i file di progetto.

---
+++ Array_e_funzioni/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_e_funzioni
+{
+    internal class Program
+    {
+        static int aggiunta_in_coda()
+        {
+            return 123;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[0];
+            //aggiunta di un elemento all'array in coda
+            //crea stringa con codice html per visualizzare l'array
+            //funzione per cercare un elemento dell'array, restituire o la posizione o se non possibile -1
+            //cancellazione di un elemento da un array
+            //Inserimento di un valore in una posizione dell'array.
+        }
+    }
+}
